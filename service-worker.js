ffc10e932180a410c48b16bd37c549a9d08eb8cd
diegomolinacastrovet-droid <diegomolinacastrovet@gmail.com>
2026-07-14 23:38:14 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const CACHE = "vademecum-v1";
+
+const FILES = [
+  "./"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
